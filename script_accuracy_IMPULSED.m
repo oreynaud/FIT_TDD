d6441b3573d9188d772ec04e8b6f371116201100
oreynaud <33052428+oreynaud@users.noreply.github.com>
2017-10-24 15:46:53 +0200

    Add files via upload

--- script_accuracy_IMPULSED.m
+++ script_accuracy_IMPULSED.m
@@ -1,8 +1,10 @@
 % generate frequency list (Jiang et al, MRM 2016)
 f=[0 50 100 150]; f=[f f f f f]; % list of frequencies (x number of b-values)
 
-% best parameters for HCT116 tumors (Jiang et al, MRM 2016 - table 2)
-p=[0.46 13.51/2 1.53 0.75 2.36]; 
+% best parameters for tumors from (Jiang et al, MRM 2016 - table 2)
+p=[0.46 13.51/2 1.53 0.75 2.36]; % for HCT116 cell line
+% p=[0.86 18.95/2 1.15 0.44 2.13]; % for DiFi cell line
+% p=[0.49 11.52/2 0.87 0.80 5.67]; % for SW620 cell line
 
 % generate synthetic tumor signal
 F_true=fit_impulsed(p,f);
